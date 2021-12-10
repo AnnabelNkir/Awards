797d373963e8aa486c0cc6d2114ee70281eb45bf
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-10 21:52:16 +0300

    This commit updates the navbar

--- awardsproject/urls.py
+++ awardsproject/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth import views 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+# from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('awardsapp.urls')),
+    # # path('accounts/register/', RegistrationView.as_view(success_url='/'),
+    #  name='django_registration_register'),
+    # path('accounts/', include('django_registration.backends.one_step.urls')),
+    # path('logout/', views.logout_then_login, name='logout'), 
+    
+    
 ]
