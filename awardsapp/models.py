676b7f2a317a8d8f1fa0a62d9396d7ae8fbbeaf8
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-13 15:08:29 +0300

    This commit adds the raings model

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -56,3 +56,16 @@ class Comment(models.Model):
     class Meta:
         ordering = ["-id"]
 
+class Rates(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post =  models.ForeignKey(Projects,on_delete=models.CASCADE,related_name='likes')
+    design = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
+    usability = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)],null=True)
+    creativity = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
+    content = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
+
+    def save_rate(self):
+            self.save()
+
+    def delete_rate(self):
+        self.delete()
