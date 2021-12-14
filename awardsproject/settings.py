d996faad0a1f232f01a2c4dd750db5c46b7fe71d
AnnabelNkir <nkiroteannabel@gmail.com>
2021-12-15 00:20:54 +0300

    Deployment to heroku

--- awardsproject/settings.py
+++ awardsproject/settings.py
@@ -61,7 +61,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['https://awardshubspot.herokuapp.com/']
 LOGIN_REDIRECT_URL = 'home'
 
 REST_FRAMEWORK = {
